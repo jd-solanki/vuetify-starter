bc4ec75b3cdf5162a59b674cca210e5b7c89fc3d
JD Solanki <jdsolanki0001@gmail.com>
2022-09-06 12:59:09 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router';
+</script>
+
+<template>
+  <VApp>
+
+    <VMain class="wrapper">
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+
+      <VBtn color="primary">Awesome</VBtn>
+    </VMain>
+  </VApp>
+
+  <RouterView />
+</template>
